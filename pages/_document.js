42648f0fce7f3d05d146669ed7634a8bd8650980
Syed Asif <sydasif78@gmail.com>
2025-06-03 20:48:10 +0500

    Add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,11 @@ export default function Document() {
           href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/themes/prism-okaidia.min.css"
           rel="stylesheet"
         />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <body>
         <Main />
